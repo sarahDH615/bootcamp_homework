fae6bc30618af16d9ca0c56bf6d94bd8200a0481
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2020-11-25 11:59:14 -0500

    used dictionaries w/in Homework3 main.py files

--- Homework3/PyBank/main.py
+++ Homework3/PyBank/main.py
@@ -1,31 +1,39 @@
 import os
 import csv
 
-#lists to hold values from col1(row[0]) and col2(row[1])
+#lists to hold values from months: col1(row[0]) and profits/losses: col2(row[1])
+#variables to hold max inc/dec, set to zero
+#dictionary for storing summary at end
+#dictionary entry header
 months = []
 money_made = []
 max_inc = 0
 max_dec = 0
-#categories = []
-#values1 = []
-#values2 = []
-
-#functions to call for finding values from the lists
+bank_dictionary = {}
+bank_dictionary ["Financial Analysis"] = " "
 
 
+#opening csv file
 csvpath = os.path.join("Resources", "budget_data.csv")
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
-
+    
+    #for loop to collect the months and money info
     for row in csvreader:
+        #skip headers w/ continue
+        #if not header, append the month to months list
         if row[0] == "Date":
             continue
         months.append(row[0])
         if row[1] == "Profit/Losses":
             continue
+        #if not header, append the money to money list
         money_made_int = int(row[1])
         money_made.append(money_made_int)
+        #wrap row[1] in integer cast so it can be compared to numerical values
+        #add money to max_inc/_dec depending on whether they are greater or less than zero/last max value
+        #retrieve row[0] for the current max value
         if int(row[1]) > max_inc:
             max_inc = int(row[1])
             max_inc_month = row[0]
@@ -33,69 +41,48 @@ with open(csvpath) as csvfile:
             max_dec = int(row[1])
             max_dec_month = row[0]
     
-    #print(months)
-    #print(money_made)
-    #print(max_inc)
-    #print(max_inc_month)
-    #print(max_dec)
-    #print(max_dec_month)
 
-
+#print starting info
 print("                   ")
 print("Financial Analysis:")
 print("______________________________________")    
 print("                   ")
 
+#make an integer out of the length of the months list
 months_total = int(len(months))
+#add entry to dictionary for total months
+#print total months
+bank_dictionary ["Total Months"] = months_total
 print(f"Total Months: {months_total}")
-#values1.append(int(len(months)))
 
-#def months_total(a_list):
-#    print(f"Total Months: {int(len(a_list))}")
-#months_total(months)
-
-
+#sum money made list
 net_total = sum(money_made)
+#add entry to dictionary for net total
+#print net total
+bank_dictionary ["Net Total"] = net_total
 print(f"Net Total: ${net_total} ")
-#values1.append(net_total)
 
-#def net_total(a_list):
-    #print(f"Net Total: ${(sum(a_list))} ")
-#net_total(money_made)
-
+#calc avg change, round it to 2 decimal places
 avg_change = round((net_total/months_total), 2)
+#add entry to dictionary for avg change
+#print avg change
+bank_dictionary ["Average Change"] = avg_change
 print(f"Average Change: ${avg_change}")
-#values1.append(avg_change)
 
-#def avg_change_calc():
-    #months_total/net_total
-
+#add entries to dictionary for max_inc/_dec and their months
+#print max_inc/_dec and months
+bank_dictionary ["Greatest Increase in Profits"] = max_inc_month, max_inc
 print(f"Greatest Increase in Profits: {max_inc_month} (${max_inc})")
+bank_dictionary ["Greatest Decrease in Profits"] = max_dec_month, max_dec
 print(f"Greatest Decrease in Profits: {max_dec_month} (${max_dec})")
 
-#values1.append(max_inc_month)
-#values1.append(max_dec_month)
-
-#values2.append(max_inc)
-#values2.append(max_dec)
-
-#categories = ["Total Months:", "Net Total:", "Average Change:", "Greatest Increase in Profits:", "Greatest Decrease in Profits:"]
-
-#def max_val(a_list):
-    #currently displaying wrong max number, needs to incl the date
-    #print(f"Maximum Increase in Profits: ${max(a_list)}")
-#max_val(money_made)
-#def min_val():
-#    min(money_made)
+#print end info
 print("                   ")
 print("______________________________________")    
 print("(End of 'Financial Analysis'")
 print("                   ")
 
-
-#2 col: [0] is heading; [1] is data
-
-
+#create a path for output file, open it
 output_path = os.path.join("Analysis", "pybank_results.csv")
 
 with open(output_path, 'w', newline='') as csvfile:
@@ -103,26 +90,6 @@ with open(output_path, 'w', newline='') as csvfile:
     # Initialize csv.writer
     csvwriter = csv.writer(csvfile, delimiter=',')
 
-    csvwriter.writerow(["Financial Analysis"])
-
-    csvwriter.writerow(["Total Months:", "86"])
-
-    csvwriter.writerow(["Net Total:", "$38382578"])
-
-    csvwriter.writerow(["Average Change:", "$446309.05"])
-
-    csvwriter.writerow(["Greatest Increase in Profits:", "Feb-2012" ,"$1170593"])
-
-    csvwriter.writerow(["Greatest Decrease in Profits:", "Sep-2013" ,"$-1196225"])
-
-
-
-
-
-    
-
-
-
-
-
-       
+    #for loop to print a key and value(s) pair from bank_dict in each row of the csvwriter
+    for key, value in bank_dictionary.items():
+        csvwriter.writerow([key, value])
